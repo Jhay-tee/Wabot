7cae680367a499fe1b5fa776e35e5fb4e8f3a1fa
Jhay-tee <akpanjonathan2006@gmail.com>
2026-03-19 23:01:35 +0100

    Updated server fails

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const WA_TABLE = "wa_sessions";
 const BOT_TIMEZONE = "Africa/Lagos";
 
 // Working version for Baileys 6.7.2
-const BAILEYS_VERSION = [2, 2413, 1];
+const BAILEYS_VERSION = undefined;
 
 const VULGAR_WORDS = [
   "fuck","fucking","fucker","fucked",
